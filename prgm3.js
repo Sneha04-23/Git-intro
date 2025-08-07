c6cca7086f0ba3c2a1d1820953ec92fba5d009aa
Sneha04-23 <sneha.k.stackup@gmail.com>
2025-08-08 00:24:11 +0530

    Git intro

---
+++ prgm3.js
@@ -0,0 +1,14 @@
+let num=1234567890;
+ let oddCount=0;
+ let evenCount=0;
+ while(num>0){
+    let lastDigit = num%10
+    if(lastDigit%2==0){
+        evenCount++
+    }
+    else{
+        oddCount++
+    }
+    num=Math.floor(num/10)
+ }
+ console.log("Even Count:",evenCount,"Odd Count:",oddCount)
